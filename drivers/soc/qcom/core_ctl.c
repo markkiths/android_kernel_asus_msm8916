fa612c6f3f9d259b8bfb34e1945796c9a1ecd147
markkiths <markkiths@gmail.com>
2022-07-12 14:15:08 +0000

    Revert "Revert "Revert "core_ctl: Update current->comm detection for Oreo"""
    This reverts commit b559532daf6788af3eeb434c219375190ec7f3c7.

--- drivers/soc/qcom/core_ctl.c
+++ drivers/soc/qcom/core_ctl.c
@@ -107,9 +107,7 @@ static ssize_t store_min_cpus(struct cpu_data *state,
 		return -EINVAL;
 
 	if (!strncmp(current->comm, "system_server", 13) ||
-			!strncmp(current->comm, "PowerManager", 12) ||
-			!strncmp(current->comm, "Binder", 6) ||
-			!strncmp(current->comm, "InputReader", 11))
+			!strncmp(current->comm, "PowerManager", 12))
 		return -EINVAL;
 
 	state->min_cpus = min(val, state->max_cpus);
@@ -132,9 +130,7 @@ static ssize_t store_max_cpus(struct cpu_data *state,
 		return -EINVAL;
 
 	if (!strncmp(current->comm, "system_server", 13) ||
-			!strncmp(current->comm, "PowerManager", 12) ||
-			!strncmp(current->comm, "Binder", 6) ||
-			!strncmp(current->comm, "InputReader", 11))
+			!strncmp(current->comm, "PowerManager", 12))
 		return -EINVAL;
 
 	val = min(val, state->num_cpus);
